5c175d45eab7f121f84fe188ebd5fafaeeb5b36f
jiho Shin <jhhj424@naver.com>
2021-07-26 23:46:51 +0900

    refactor: KafkaProducerController 클래스 네이밍 수정

--- src/main/java/com/example/kafkademo/api/KafkaProducerController.java
+++ src/main/java/com/example/kafkademo/api/KafkaProducerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class kafkaProducerController {
+public class KafkaProducerController {
 
     private final KafkaProducer kafkaProducer;
 
