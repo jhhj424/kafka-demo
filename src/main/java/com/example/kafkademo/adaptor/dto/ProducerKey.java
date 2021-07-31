f7ddb8611fb891152a5ef7fcac3667339499b9d2
jiho Shin <jhhj424@naver.com>
2021-07-31 23:22:58 +0900

    feat: producer key & value 추가

---
+++ src/main/java/com/example/kafkademo/adaptor/dto/ProducerKey.java
@@ -0,0 +1,8 @@
+package com.example.kafkademo.adaptor.dto;
+
+import lombok.Data;
+
+@Data
+public class ProducerKey {
+    private long id;
+}
---
+++ src/main/java/com/example/kafkademo/adaptor/dto/ProducerValue.java
@@ -0,0 +1,9 @@
+package com.example.kafkademo.adaptor.dto;
+
+import lombok.Data;
+
+@Data
+public class ProducerValue {
+    private String name;
+    private String email;
+}
